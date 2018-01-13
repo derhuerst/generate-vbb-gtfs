1241af3da941259638212d8843794fd62a07ab35
Jannis R <mail@jannisr.de>
2018-01-13 23:45:43 +0100

    merge-vbb-stations -> merged-vbb-stations

--- build/merge-stations.js
+++ build/merge-stations.js
@@ -1,44 +1,41 @@
 'use strict'
 
-const keyMap = require('key-map')
 const stations = require('vbb-stations/full.json')
-const analyze = require('merge-vbb-stations')
+const mapping = require('merged-vbb-stations')
+const instructions = require('merged-vbb-stations/instructions.json')
 
 const mergeStations = () => {
-	const mapping = keyMap(Object.keys(stations))
 	const nrOfStationsBefore = Object.keys(stations).length
+	const mergedStations = Object.assign({}, stations)
 
-	for (let id1 in stations) {
-		const s1 = stations[id1]
-		for (let id2 in stations) {
-			if (id1 === id2) continue
-			const s2 = stations[id2]
-
-			const res = analyze(s1, s2)
-			if (!res) continue
-			const {op, src, dest, useStationName} = res
-			if (op !== analyze.MERGE) {
-				console.error('unsupported merge operation: ' + op)
-				continue
-			}
-
-			const name = useStationName ? dest.name : src.name
-			console.info(src.id, src.name, 'as', name, 'into', dest.id, dest.name)
-
-			for (let stop of src.stops) {
-				stop.station = dest.id
-				if (useStationName) stop.name = dest.name
-			}
-			dest.stops = dest.stops.concat(src.stops)
-			mapping.map(src.id, dest.id)
-			delete stations[src.id]
+	for (let instruction of instructions) {
+		if (instruction.op !== 'merge') {
+			console.error('unsupported merge operation', op)
+			continue
 		}
+
+		const {dest, stopName} = instruction
+		const src = stations[instruction.src.id]
+		console.info(src.id, src.name, 'as', stopName, 'into', dest.id, dest.name)
+
+		let newDest = mergedStations[dest.id]
+		if (!newDest) newDest = mergedStations[dest.id] = Object.assign({}, dest)
+
+		for (let stop of src.stops) {
+			stop.station = dest.id
+			stop.name = stopName
+		}
+		newDest.stops = dest.stops.concat(src.stops)
+		delete mergedStations[src.id]
 	}
 
-	const nrOfStationsAfter = Object.keys(stations).length
+	const nrOfStationsAfter = Object.keys(mergedStations).length
 	console.error('removed', nrOfStationsBefore - nrOfStationsAfter, 'stations')
 
-	return Promise.resolve({stations, map: mapping.toObject()})
+	return Promise.resolve({
+		stations: mergedStations,
+		map: mapping
+	})
 }
 
 module.exports = mergeStations
